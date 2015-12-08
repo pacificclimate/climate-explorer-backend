c1cff6ddde4df8f57050288dddb2409cf016a53a
James Hiebert <james@hiebert.name>
2015-12-08 13:57:06 -0800

    Added a pre-masked (i.e. realistic) raster file to the testing data for ensuring that get_array handles non-masked and masked base data

--- ce/tests/test_util.py
+++ ce/tests/test_util.py
@@ -1,6 +1,7 @@
 from pkg_resources import resource_filename
 
 import pytest
+import numpy as np
 from numpy.ma import MaskedArray
 
 from ce.api.util import get_array
@@ -8,11 +9,11 @@ from ce.api.util import get_array
 @pytest.mark.parametrize(('fname', 'var'), (
     ('cgcm.nc', 'tasmax'),
     ('cgcm-tmin.nc', 'tasmin'),
-    ('CanESM2-rcp85-tasmax-r1i1p1-2010-2039.nc', 'tasmax')
-    # FIXME: Need to test a file with masked values
+    ('prism_pr_small.nc', 'pr') # a file with masked values
 ))
 def test_get_array(fname, var):
     fname = resource_filename('ce', 'tests/data/' + fname)
     x = get_array(fname, 0, "", var)
     assert type(x) == MaskedArray
     assert hasattr(x, 'mask')
+    assert np.mean(x) > 0
