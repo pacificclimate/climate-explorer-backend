ea6ac68f3ddec03c9348e4fb1bc7dfa0360dceea
Basil Veerman <bveerman@uvic.ca>
2015-12-08 19:16:49 -0800

    Test climo file generation to python script

--- data_prep/generate_climos.py
+++ data_prep/generate_climos.py
@@ -1,6 +1,8 @@
 import os, os.path
 import re
 
+from cdo import Cdo
+
 def iter_matching(dirpath, regexp):
     # http://stackoverflow.com/questions/4639506/os-walk-with-regex
     """Generator yielding all files under `dirpath` whose absolute path
@@ -13,13 +15,21 @@ def iter_matching(dirpath, regexp):
     for dir_, dirnames, filenames in os.walk(dirpath):
         for filename in filenames:
             abspath = os.path.join(dir_, filename)
-            print abspath
             if regexp.match(abspath):
                 yield abspath
 
 def main():
-    files = iter_matching('/home/data/climate/CMIP5/CCCMA/CanESM2/', re.compile('.*rcp.*tasmax.*r1i1p1.*nc'))
-    print [x for x in files]
+    test_files = iter_matching('/home/data/climate/CMIP5/CCCMA/CanESM2/', re.compile('.*rcp.*tasmax.*r1i1p1.*nc'))
+
+    cdo = Cdo()
+
+    for fp in test_files:
+        print fp
+        temp = r'~/deleteme_temp.nc'
+        ofile = r'~/deleteme_out.nc'
+        cdo.seldate('2010-01-01,2039-12-31', input=fp, output=temp)
+        cdo.copy(input='-ymonmean {} -yseasmean {} -timmean {}'.format(temp, temp, temp), output=ofile)
+
 
 if __name__ == '__main__':
     main()
