131fd664dfa9449c012872973d068abe94a0f7a1
Basil Veerman <bveerman@uvic.ca>
2016-01-11 18:59:10 -0800

    Add memoizing wrapper to mask generation

--- ce/api/geo.py
+++ ce/api/geo.py
@@ -1,6 +1,57 @@
+import logging
+import collections
+from collections import OrderedDict
+from threading import RLock
+
 import numpy as np
 import numpy.ma as ma
 from shapely.geometry import Polygon, Point, CAP_STYLE
+from shapely.wkt import loads
+
+log = logging.getLogger(__name__)
+cache_lock = RLock()
+
+class memoize_mask(object):
+    '''
+    Decorator. Caches wktToMask keyed to model_id and the WKT string
+    '''
+    def __init__(self, func, maxsize=100):
+        '''
+        Args:
+            func: the function to wrap
+            maxsize (int): Max number of function calls to cache
+        '''
+
+        self.hits = self.misses = 0
+        self.func = func
+        self.maxsize = maxsize
+        self.cache = OrderedDict()
+
+    def __call__(self, *args):
+        # Set key to model_id and wkt polygon
+        key = (args[0].model_id, args[1])
+        log.debug('Checking cache for key {}'.format(key))
+
+        with cache_lock:
+            try:
+                result = self.cache[key]
+                self.cache.move_to_end(key) # record recent use of this key
+                self.hits += 1
+                log.debug('Cache HIT')
+                return result
+            except KeyError:
+                pass
+
+        log.debug('Cache MISS')
+        result = self.func(*args)
+
+        with cache_lock:
+            self.cache[key] = result
+            self.misses += 1
+            if len(self.cache) > self.maxsize:
+                self.cache.popitem(0) # Purge least recently used cache entry
+
+        return result
 
 def pointInPoly(x, y, poly):
     if x is ma.masked or y is ma.masked:
@@ -13,6 +64,11 @@ def pointInPoly(x, y, poly):
         return ma.masked
 
 pointsInPoly = np.vectorize(pointInPoly)
+
+@memoize_mask
+def wktToMask(nc, wkt):
+    poly = loads(wkt)
+    return polygonToMask(nc, poly)
 
 def polygonToMask(nc, poly):
 
