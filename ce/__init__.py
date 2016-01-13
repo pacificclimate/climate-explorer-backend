e554deade75f96c282b8415f015b3b5b0d423b21
Basil Veerman <bveerman@uvic.ca>
2016-01-12 16:55:38 -0800

    Add Flask-Cache memoizing the multistats

--- ce/__init__.py
+++ ce/__init__.py
@@ -2,12 +2,16 @@ import os
 
 from flask import Flask
 from flask.ext.cors import CORS
+from flask.ext.cache import Cache
+
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 from ce.views import add_routes
 
 def get_app():
     app = Flask(__name__)
     CORS(app)
+    cache.init_app(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
         'MDDB_DSN', 'postgresql://httpd_meta@monsoon.pcic.uvic.ca/pcic_meta')
     add_routes(app)
