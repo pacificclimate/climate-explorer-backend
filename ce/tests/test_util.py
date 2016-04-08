e0cd3d03d6e7a38a8f5bd8322b3930edb454a6ad
James Hiebert <james@hiebert.name>
2016-04-08 10:43:22 -0700

    Adds a performance test on ce.util.get_array()

--- ce/tests/test_util.py
+++ ce/tests/test_util.py
@@ -1,6 +1,7 @@
 from pkg_resources import resource_filename
 from datetime import timezone
 from datetime import datetime
+from time import time
 
 import pytest
 import numpy as np
@@ -12,14 +13,19 @@ from ce.api.util import get_array, mean_datetime
 @pytest.mark.parametrize(('fname', 'var'), (
     ('cgcm.nc', 'tasmax'),
     ('cgcm-tmin.nc', 'tasmin'),
-    ('prism_pr_small.nc', 'pr') # a file with masked values
+    ('prism_pr_small.nc', 'pr'), # a file with masked values
+    ('bccaq_tnx.nc', 'tnxETCCDI'),
 ))
-def test_get_array(fname, var):
+def test_get_array(fname, var, polygon):
     fname = resource_filename('ce', 'tests/data/' + fname)
-    x = get_array(fname, 0, "", var)
+    t0 = time()
+    x = get_array(fname, 0, polygon, var)
+    t = time() - t0
+    print(t)
+    assert t < .030
     assert type(x) == MaskedArray
     assert hasattr(x, 'mask')
-    assert np.mean(x) > 0
+    assert np.mean(x) > 0 or np.all(x.mask)
 
 utc = timezone.utc
 
