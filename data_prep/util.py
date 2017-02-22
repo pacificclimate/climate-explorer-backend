2927299b5c85b25ea4a841c0fcaeeca4c58b78c3
Rod Glover <rglover@uvic.ca>
2017-02-22 14:03:17 -0800

    Make date-to-string functions work for date.year < 1900

--- data_prep/util.py
+++ data_prep/util.py
@@ -10,12 +10,16 @@ def ss2d(s):
     return datetime.strptime(s, '%Y%m%d')
 
 
-def d2s(d):
-    return datetime.strftime(d, '%Y-%m-%d')
+def d2s(date):
+    '''Equivalent of datetime.strftime(d, '%Y-%m-%d'), but
+    gets around the idiotic Python 2.7 strftime limitation of year >= 1900'''
+    return '{y}-{m}-{d}'.format(y=str(date.year), m=str(date.month).zfill(2), d=str(date.day).zfill(2))
 
 
-def d2ss(d):
-    return datetime.strftime(d, '%Y%m%d')
+def d2ss(date):
+    '''Equivalent of datetime.strftime(d, '%Y%m%d'), but
+    gets around the idiotic Python 2.7 strftime limitation of year >= 1900'''
+    return '{y}{m}{d}'.format(y=str(date.year), m=str(date.month).zfill(2), d=str(date.day).zfill(2))
 
 
 
