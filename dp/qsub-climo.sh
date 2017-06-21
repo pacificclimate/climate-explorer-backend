6e7196bf2e6eff9c973bcbecf4f7c76745150f11
Rod Glover <rglover@uvic.ca>
2017-06-21 16:42:36 -0700

    Put climo outputs into separate dirs

--- dp/qsub-climo.sh
+++ dp/qsub-climo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Submit a job to the pbs queue that runs generate_climos on a single NetCDF file.
 # Arguments:
-# $1: destination directory for climo output files and log files
-# $2: input NetCDF file path
+# $1: destination base directory for climo output files and log files;
+#     log files are placed in the base directory
+#     climo output files are placed in a subdirectory of the base directory named by the PBS job id
+# $2: NetCDF file path
 
 # Create a heredoc containing the required script, and submit it via qsub.
 # All qsub arguments are supplied via PBS directives inside the heredoc.
@@ -16,6 +18,8 @@ cat <<EOF | qsub
 #PBS -N generate_climos
 #PBS -d /storage/home/rglover/code/climate-explorer-backend
 
+pbs_job_num=\$(expr match "\$PBS_JOBID" '\([0-9]*\)')
+
 # Set up the execution environment
 module load netcdf-bin
 module load cdo-bin
@@ -27,12 +31,13 @@ mkdir -p \$indir && cp $2 \$indir
 infile=\$indir/$(basename $2)
 
 # Output directory is automatically created by generate_climos
-outdir=\$TMPDIR/climo/output
+baseoutdir=\$TMPDIR/climo/output
+outdir=\$baseoutdir/\$pbs_job_num
 
 # Generate climo means
 python dp/generate_climos.py --split-vars --split-intervals -o \$outdir \$infile
 
 # Copy result file to final destination and remove temporary input file
-rsync \$outdir $1
+rsync -r \$baseoutdir $1
 rm \$infile
 EOF
