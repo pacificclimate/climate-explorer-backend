95667b2f05019b8991db1a5b9fc162759a749110
Rod Glover <rglover@uvic.ca>
2017-06-29 09:19:49 -0700

    Add no-work test-job option to gcsub

--- dp/jobqueueing/gcsub.py
+++ dp/jobqueueing/gcsub.py
@@ -24,6 +24,45 @@ from dp.jobqueueing.jobqueueing_db import GenerateClimosQueueEntry
 logger = default_logger()
 
 
+def make_qsub_test_script(queue_entry):
+    return '''
+#PBS -l nodes=1:ppn={queue_entry.ppn}
+#PBS -l vmem={vmem}mb
+#PBS -l walltime={queue_entry.walltime}
+#PBS -o {queue_entry.output_directory}
+#PBS -e {queue_entry.output_directory}
+#PBS -m abe
+#PBS -N generate_climos:{input_filename}
+#PBS -d /storage/home/rglover/code/climate-explorer-backend
+
+pbs_job_num=$(expr match "$PBS_JOBID" '([0-9]*)')
+
+# Set up the execution environment
+module load netcdf-bin
+module load cdo-bin
+source py3.4/bin/activate
+
+# Copy NetCDF file to $TMPDIR/climo/input
+indir=$TMPDIR/climo/input
+echo mkdir -p $indir && cp {queue_entry.input_filepath} $indir
+infile=$indir/{input_filename}
+
+# Output directory is automatically created by generate_climos
+baseoutdir=$TMPDIR/climo/output
+outdir=$baseoutdir/$pbs_job_num
+
+# Generate climo means
+echo python dp/generate_climos.py -o $outdir $infile
+
+# Copy result file to final destination and remove temporary input file
+# Since output files are small, we're not removing them here.
+echo rsync -r $baseoutdir {queue_entry.output_directory}
+echo rm $infile
+            '''.format(queue_entry=queue_entry,
+                       input_filename=os.path.basename(queue_entry.input_filepath),
+                       vmem=queue_entry.ppn * 12000)
+
+
 def make_qsub_script(queue_entry):
     return '''
 #PBS -l nodes=1:ppn={queue_entry.ppn}
@@ -72,7 +111,11 @@ def submit_generate_climos_pbs_jobs(session, args):
 
     for entry in entries:
         qsub = Popen(['qsub'], stdin=PIPE, stdout=PIPE)
-        pbs_job_id, error = qsub.communicate(input=make_qsub_script(entry).encode('utf-8'))
+        if args.test_job:
+            input = make_qsub_test_script(entry).encode('utf-8')
+        else:
+            input = make_qsub_script(entry).encode('utf-8')
+        pbs_job_id, error = qsub.communicate(input=input)
         if error:
             entry.status = 'ERROR'
             logger.error('qsub failed: {}'.format(error))
@@ -94,7 +137,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Submit file(s) from generate_climos queue to PBS jobs queue')
-    add_global_arguments(parser)
+    group = add_global_arguments(parser)
+    group.add_argument('--test-job', dest='test_job', action='store_true',
+                       help='Submit a test job that performs no work')
 
     group = parser.add_argument_group('Submission arguments')
     group.add_argument('-n', '--number', type=int, dest='number', default=1,
