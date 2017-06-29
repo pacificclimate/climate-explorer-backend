8e2818e91daf47b9f8a761efab2b24787bc93286
Rod Glover <rglover@uvic.ca>
2017-06-29 11:18:01 -0700

    Enable CLI database selection in alembic

--- dp/jobqueueing/alembic/env.py
+++ dp/jobqueueing/alembic/env.py
@@ -1,3 +1,24 @@
+"""
+Customized alembic environment manager.
+
+This manager enables command-line specification of the database to migrate.
+This simplifies testing and use in various environments, e.g., local dev machine vs. compute node
+(with test and prod databases on compute node).
+
+The database to migrate is specified using the command-line argument `-x`, as follows::
+
+    alembic -x db=<db-name> upgrade ...
+
+This syntax is now required; i.e., `-x db=<db-name>` cannot be omitted from the command.
+
+The `<db-name>` section must appear in `alembic.ini`, with a `sqlalchemy.url =` line.
+For example, to use `alembic -x db=test upgrade ...`::
+
+    [test]
+    sqlalchemy.url = sqlite:////path/to/database/test.sqlite
+
+Adapted from https://gist.github.com/twolfson/4bc5813b022178bd7034
+"""
 from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
@@ -21,6 +42,14 @@ target_metadata = jobqueueing_db.Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+# Obtain command-line specification of database to run migration against.
+cmd_kwargs = context.get_x_argument(as_dictionary=True)
+if 'db' not in cmd_kwargs:
+    raise Exception('We couldn\'t find `db` in the CLI arguments. '
+                    'Please verify `alembic` was run with `-x db=<db_name>` '
+                    '(e.g. `alembic -x db=development upgrade head`)')
+db_name = cmd_kwargs['db']
 
 
 def run_migrations_offline():
@@ -50,8 +79,14 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    # Load db config on top of alembic config. This enables CLI spec of database to migrate.
+    alembic_config = config.get_section(config.config_ini_section)
+    db_config = config.get_section(db_name)
+    for key in db_config:
+        alembic_config[key] = db_config[key]
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        alembic_config,
         prefix='sqlalchemy.',
         poolclass=pool.NullPool)
 
