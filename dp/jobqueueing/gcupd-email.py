13ed9069d8136b56dc7158c3a699d60fbbe1f159
Rod Glover <rglover@uvic.ca>
2017-07-12 14:27:50 -0700

    Improve code and doc of gcupd-email

--- dp/jobqueueing/gcupd-email.py
+++ dp/jobqueueing/gcupd-email.py
@@ -1,10 +1,16 @@
 """
-Script to update the status of a queue entry based on the content of a standard status email sent by PBS.
+Script to update the status of a queue entry based on the content of a
+standard status email sent by PBS. Contents of the status email is read
+from stdin.
 
-The email is read from stdin and scanned for information that identifies the job and its status,
-and the queue entry is found and updated accordingly.
+NOTE: The canny user will prefer to update using `gcupd-qstat.py` unless they
+have waited to long and the data has been cleared from PBS's queue.
 
-WARNING: Currently this script does not enforce any ordering of begin and terminate status changes.
+The email is read from stdin and scanned for information that identifies the
+job and its status, and the queue entry is found and updated accordingly.
+
+WARNING: This script does not enforce any ordering of start and terminate
+status changes.
 """
 
 from argparse import ArgumentParser
@@ -25,12 +31,21 @@ logger = default_logger()
 
 
 def update_generate_climos_queue_from_status_email(session, status_email):
+    """
+    Find and update the status of a queue entry based on the content of a
+    standard status email sent by PBS.
+
+    :param session: database session
+    :param status_email (str): contents of status email, line breaks preserved
+    :return (int): exit status code (0 for successful processing of email)
+    """
     # Extract important info from email
     pbs_job_id = action = exit_status = None
     match = re.search(r'^PBS Job Id: *(.*)$', status_email, flags=re.MULTILINE)
     if match:
         pbs_job_id = match.group(1)
-    match = re.search(r'(Begun) execution|Execution (terminated)', status_email, flags=re.MULTILINE)
+    match = re.search(r'(Begun) execution|Execution (terminated)', status_email,
+                      flags=re.MULTILINE)
     if match:
         action = match.group(1) or match.group(2)
     match = re.search(r'Exit_status=(\d+)', status_email, flags=re.MULTILINE)
@@ -41,12 +56,15 @@ def update_generate_climos_queue_from_status_email(session, status_email):
         logger.error('Could not find PBS job id in status email. Skipping.')
         return 1
 
-    entry = session.query(GenerateClimosQueueEntry)\
-        .filter(GenerateClimosQueueEntry.pbs_job_id == pbs_job_id)\
+    entry = (
+        session.query(GenerateClimosQueueEntry)
+        .filter(GenerateClimosQueueEntry.pbs_job_id == pbs_job_id)
         .first()
+    )
 
     if not entry:
-        logger.error('Could not find generate_climos queue entry corresponding to PBS job id {}. Skipping.'
+        logger.error('Could not find generate_climos queue entry corresponding '
+                     'to PBS job id {}. Skipping.'
                      .format(pbs_job_id))
         return 1
 
@@ -56,7 +74,8 @@ def update_generate_climos_queue_from_status_email(session, status_email):
 
     elif action == 'terminated':
         if not exit_status:
-            logger.error('Could not determine PBS job exit status from status email. Skipping.')
+            logger.error('Could not determine PBS job exit status from '
+                         'status email. Skipping.')
             return 1
 
         if exit_status == '0':
@@ -67,7 +86,8 @@ def update_generate_climos_queue_from_status_email(session, status_email):
         entry.completion_message = status_email
 
     else:
-        logger.error('Could not determine action (begin, terminate) from status email. Skipping.')
+        logger.error('Could not determine action (begin, terminate) from '
+                     'status email. Skipping.')
         return 1
 
     session.commit()
@@ -79,11 +99,13 @@ def main(args):
     engine = create_engine(dsn)
     session = sessionmaker(bind=engine)()
 
-    update_generate_climos_queue_from_status_email(session, '\n'.join(sys.stdin))
+    update_generate_climos_queue_from_status_email(
+        session, '\n'.join(sys.stdin))
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Update generate_climos queue using PBS status email')
+    parser = ArgumentParser(
+        description='Update generate_climos queue using PBS status email')
     add_global_arguments(parser)
     args = parser.parse_args()
     logger.setLevel(getattr(logging, args.loglevel))
