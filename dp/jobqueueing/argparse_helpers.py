3a018f20167534660d9cc7e6bb59b37e7d79f0a0
Rod Glover <rglover@uvic.ca>
2017-07-12 10:29:51 -0700

    PEP8-ify argparse_helpers

--- dp/jobqueueing/argparse_helpers.py
+++ dp/jobqueueing/argparse_helpers.py
@@ -8,7 +8,6 @@ from dateutil import parser as dateparser
 
 from dp.argparse_helpers import log_level_choices, strtobool
 
-
 status_choices = ['NEW', 'SUBMITTED', 'RUNNING', 'SUCCESS', 'ERROR']
 
 
@@ -20,75 +19,103 @@ def walltime(string):
 
 def add_global_arguments(parser):
     group = parser.add_argument_group('Global arguments')
-    group.add_argument('-d', '--database', help='Filepath to queue management database',
-                       default='/storage/data/projects/comp_support/climate_exporer_data_prep/climatological_means/jobqueue-prod.sqlite')
-    group.add_argument('-L', '--loglevel', help='Logging level',
+    group.add_argument(
+        '-d', '--database',
+        help='Filepath to queue management database',
+        default='/storage/data/projects/comp_support/climate_exporer_data_prep/'
+                'climatological_means/jobqueue-prod.sqlite')
+    group.add_argument(
+        '-L', '--loglevel', help='Logging level',
                        choices=log_level_choices, default='INFO')
     return group
 
 
 def add_gcadd_arguments(parser):
     group = parser.add_argument_group('Add arguments')
-    group.add_argument('-f', '--force', action='store_true',
-                       help='Force addition of a new queue entry even if one for this input filename already exists')
+    group.add_argument(
+        '-f', '--force', action='store_true',
+        help='Force addition of a new queue entry even if one for this '
+             'input filename already exists')
     return group
 
 
 def add_generate_climos_arguments(parser, o_required=True, flag_default=True):
     group = parser.add_argument_group('generate_climos arguments')
-    group.add_argument('input_filepath', help='File to queue')
-    group.add_argument('-o', '--output-directory', required=o_required, dest='output_directory',
-                       help='Path to directory where output files will be placed')
-    group.add_argument('-g', '--convert-longitudes', type=strtobool, dest='convert_longitudes', default=flag_default,
-                       help='Transform longitude range from [0, 360) to [-180, 180)')
-    group.add_argument('-v', '--split-vars', type=strtobool, dest='split_vars', default=flag_default,
-                       help='Generate a separate file for each dependent variable in the file')
-    group.add_argument('-i', '--split-intervals', type=strtobool, dest='split_intervals', default=flag_default,
-                       help='Generate a separate file for each climatological period')
+    group.add_argument(
+        'input_filepath', help='File to queue')
+    group.add_argument(
+        '-o', '--output-directory',
+        required=o_required, dest='output_directory',
+        help='Path to directory where output files will be placed')
+    group.add_argument(
+        '-g', '--convert-longitudes', type=strtobool,
+        dest='convert_longitudes', default=flag_default,
+        help='Transform longitude range from [0, 360) to [-180, 180)')
+    group.add_argument(
+        '-v', '--split-vars', type=strtobool,
+        dest='split_vars', default=flag_default,
+        help='Generate a separate file for each dependent variable in the file')
+    group.add_argument(
+        '-i', '--split-intervals', type=strtobool,
+        dest='split_intervals', default=flag_default,
+        help='Generate a separate file for each climatological period')
     return group
 
 
 def add_pbs_arguments(parser, ppn_default=1, walltime_default='10:00:00'):
     group = parser.add_argument_group('PBS arguments')
-    group.add_argument('-p', '--ppn', type=int, dest='ppn', default=ppn_default,
-                       help='Processes per node')
-    group.add_argument('-w', '--walltime', type=str, dest='walltime', default=walltime_default,
-                       help='Maximum wall time')
+    group.add_argument(
+        '-p', '--ppn', type=int,
+        dest='ppn', default=ppn_default,
+        help='Processes per node')
+    group.add_argument(
+        '-w', '--walltime', type=str,
+        dest='walltime', default=walltime_default,
+        help='Maximum wall time')
     return group
 
 
 def add_submit_arguments(parser):
     group = parser.add_argument_group('Submit arguments')
-    group.add_argument('-n', '--number', type=int, dest='number', default=1,
-                       help='Number of files to submit')
+    group.add_argument(
+        '-n', '--number', type=int, dest='number', default=1,
+        help='Number of files to submit')
     return group
 
 
 def add_ext_submit_arguments(parser):
     group = parser.add_argument_group('External submission arguments')
-    group.add_argument('-s', '--submitted', type=dateparser.parse,
-                       help='Date/time that job was submitted to PBS without use of gcsub')
-    group.add_argument('-j', '--job-id', dest='pbs_job_id', type=str,
-                       help='PBS job id of submission')
+    group.add_argument(
+        '-s', '--submitted', type=dateparser.parse,
+        help='Date/time that job was submitted to PBS without use of gcsub')
+    group.add_argument(
+        '-j', '--job-id', dest='pbs_job_id', type=str,
+        help='PBS job id of submission')
     return group
 
 
 def add_listing_arguments(parser):
     group = parser.add_argument_group('Listing control arguments')
-    group.add_argument('-f', '--full', action='store_true',
-                       help='Display full listing')
-    group.add_argument('-i', '--input-filepath', dest='input_filepath',
-                       help='Input filepath (partial match)')
-    group.add_argument('-j', '--job-id', dest='pbs_job_id', type=str,
-                       help='PBS job id of submission')
-    group.add_argument('-s', '--status', help='Status of queue entry',
-                       choices=status_choices)
+    group.add_argument(
+        '-f', '--full', action='store_true',
+        help='Display full listing')
+    group.add_argument(
+        '-i', '--input-filepath', dest='input_filepath',
+        help='Input filepath (partial match)')
+    group.add_argument(
+        '-j', '--job-id', dest='pbs_job_id', type=str,
+        help='PBS job id of submission')
+    group.add_argument(
+        '-s', '--status', help='Status of queue entry',
+        choices=status_choices)
     return group
 
 
 def add_reset_arguments(parser):
     group = parser.add_argument_group('Reset arguments')
-    group.add_argument('input_filepath', help='Input filepath (full match)')
-    group.add_argument('-s', '--status', help='Status of queue entry',
-                       choices=status_choices, default='NEW')
+    group.add_argument(
+        'input_filepath', help='Input filepath (full match)')
+    group.add_argument(
+        '-s', '--status', help='Status of queue entry',
+        choices=status_choices, default='NEW')
     return group
