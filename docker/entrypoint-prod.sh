933c204cbfa5387ff5a97adc23ce326b27b9e731
John <johnsampson@uvic.ca>
2024-05-02 11:03:46 -0700

    adjust docker file, fix permissions

--- docker/entrypoint-prod.sh
+++ docker/entrypoint-prod.sh
@@ -1,2 +1,8 @@
+# gunicorn.conf is set up so that one can tune gunicorn settings when
+# running the container by setting environment an variable
+# GUNICORN_[setting], where setting is any of the parameters in this
+# list: http://docs.gunicorn.org/en/latest/settings.html
+#
+# E.g. docker run -e GUNICORN_WORKERS=10 -e GUNICORN_PORT=8000 -e GUNICORN_BIND=0.0.0.0:8000 ...
 
 poetry run gunicorn --config docker/gunicorn.conf --log-config docker/logging.conf -b :8000 ce.wsgi:app
